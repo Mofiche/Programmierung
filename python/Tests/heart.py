bf264895e5d4519c1ed88bb4166efaeb48880bb3
Mofiche <moritzenge@hotmail.de>
2019-07-16 19:28:21 +0200

    Herzschlagen

--- python/Tests/heart.py
+++ python/Tests/heart.py
@@ -1,9 +1,9 @@
 from tkinter import Tk, IntVar, Canvas
 from numpy import sin, cos
+import time
 
 
-
-def calc_Heart():
+def calc_Heart(size):
     x_vals = []
     y_vals = []
     for x in range(0, 6283):
@@ -13,12 +13,9 @@ def calc_Heart():
         y = y / 1000
         y_vals.append((13 * cos(y)) - (5 * cos(2 * y)) - (2 * cos(3 * y)) - (cos(4 * y)))
 
-    for x in range(0,6283):
-        x_vals[x] = -x_vals[x]*20 +400
-        y_vals[x] = -y_vals[x]*20 +350
-
-
-
+    for x in range(0, 6283):
+        x_vals[x] = -x_vals[x] * size + 400
+        y_vals[x] = -y_vals[x] * size + 350
 
     data = []
     for i in range(0, len(x_vals)):
@@ -26,12 +23,29 @@ def calc_Heart():
         data.append(y_vals[i])
 
     return data
+
+
 gui = Tk()
 var = IntVar()
 gui.geometry("800x800")
 gui.title("Schatzi")
-canvas = Canvas(gui,width=800, height=800, bg="white")
+canvas = Canvas(gui, width=800, height=800, bg="white")
 canvas.pack()
-data = calc_Heart()
+data = calc_Heart(20)
 obj1 = canvas.create_polygon(data, fill='#FF0000')
+obj2 = canvas.create_text(400, 50,font = 20, text = "Ich liebe dich")
+
+
+def change_heart_size(size, zeit=0.0075):
+    canvas.coords(obj1, calc_Heart(size))
+    gui.update()
+    time.sleep(zeit)
+
+
+for i in range(0, 200):
+    change_heart_size(i/10, 0.01/(i+1))
+while(True):
+    change_heart_size(19,0.4)
+    change_heart_size(20,0.4)
+
 gui.mainloop()
