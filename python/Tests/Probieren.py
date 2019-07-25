c7d384b012be5a0c28400eb46198e8e84cd4e1a2
Mofiche <moritzenge@hotmail.de>
2019-07-25 13:33:30 +0200

    Server2

---
+++ python/Tests/Probieren.py
@@ -0,0 +1,31 @@
+from numpy import *
+
+
+def isPrime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for i in range(3, int(sqrt(number) + 1), 2):
+            if number % i == 0:
+                return False
+        return True
+    return False
+
+
+def getPrimeFactors(number):
+    factors = []
+    if isPrime(number):
+        return number
+    poss_primes = []
+    for j in range(1, number):
+        poss_primes.append(j) if isPrime(j) else None
+    for i in poss_primes:
+        factors.append(i) if number % i == 0 else None
+    return factors
+
+
+zahl = int(1E7 - 1)
+primes = getPrimeFactors(zahl)
+print(str(zahl) + "\n" + str(primes))
