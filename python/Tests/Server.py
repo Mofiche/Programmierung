9a13722bd4b1fdb38d9d84560dcacdcd8d4cb8b4
Mofiche <moritzenge@hotmail.de>
2019-07-25 13:00:59 +0200

    Server

---
+++ python/Tests/Server.py
@@ -0,0 +1,8 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('127.0.0.1', 1337))
+server_socket.listen(1)
+while True:
+    (client_socket, addr) = server_socket.accept()
+    print(client_socket.recv(2**10))
