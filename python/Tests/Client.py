99d0ed7c89d1af133f51b8a46b5bc7a8b140fea1
Mofiche <moritzenge@hotmail.de>
2019-08-14 13:02:29 +0200

    14082019

--- python/Tests/Client.py
+++ python/Tests/Client.py
@@ -1,13 +1,15 @@
+import random
 import socket
 
-### calc Primfaktoren
+number = random.randint(int(1E5),int(1E6))
 
-number = 8021
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.2.104', 1337))
-client_socket.send(bytes(str(number), 'utf8'))
-while True:
-  #  (socket, addr) = client_socket.accept()
+file = open('primfac.txt', "w+")
+for number in range(1, int(1E4)):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('192.168.2.104', 1337))
+    client_socket.send(bytes(str(number), 'utf8'))
     msg = str(client_socket.recv(2**10), 'utf8')
-    print(msg)
+    client_socket.close()
+    print(number, msg)
+    file.write(str(number) + " - " + (str(msg) if msg != [] else number) + "\n")
+file.close()
--- python/Tests/Probieren.py
+++ python/Tests/Probieren.py
@@ -1,29 +1,36 @@
-from numpy import *
+import time
 
+import playsound
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
 
-def isPrime(number):
-    if number > 1:
-        if number == 2:
-            return True
-        if number % 2 == 0:
-            return False
-        for i in range(3, int(sqrt(number) + 1), 2):
-            if number % i == 0:
-                return False
-        return True
-    return False
+def String2Morse(string):
+    string = string.upper()
+    ret = []
+    for char in string:
+        ret.append(MORSE_CODE_DICT[char])
+    return ret
+string = "Ich liebe dich Julia".replace(" ", "")
+morse = String2Morse(string)
 
-
-def getPrimeFactors(number):
-    factors = []
-    if isPrime(number):
-        return number
-    poss_primes = []
-    for j in range(1, number):
-        poss_primes.append(j) if isPrime(j) else None
-    for i in poss_primes:
-        factors.append(i) if number % i == 0 else None
-    return factors
-
-
-print(isPrime(8021658347))
+for code in morse:
+    for i in code:
+        if i == '.':
+            playsound.playsound("kurz.mp3")
+        else : playsound.playsound("lang.mp3")
+    print("\n")
+    time.sleep(0.75)
+print(string, morse)
