a8ccf00a05e97f0ac2513f449d209e958244f689
Mofiche <moritzenge@hotmail.de>
2019-09-27 16:33:28 +0200

    cleaning

--- python/Tests/test.py
+++ python/Tests/test.py
@@ -1,7 +1,14 @@
-from pynput.keyboard import Controller , Key
+from pynput.keyboard import Controller, Key
+import PyAutoGUI as p
+import time
+
 keyboard = Controller()
 mail = 'fkw.peter@gmx.de'
 pw = 'NETAbi2019'
+for i in range(5):
+    p.click(1850,50)
+    time.sleep(10)
+    p.click(1850,50)
 keyboard.type(mail)
 keyboard.press(Key.tab)
 keyboard.release(Key.tab)
