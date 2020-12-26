0605c535db51b01dc59277c936896adba9d4cb79
Mofiche <moritzenge@hotmail.de>
2020-12-26 15:28:12 +0100

    NN

--- python/NN/CatOrDog.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-from torchvision import datasets, transforms
-import torch.optim as optim
-import os
